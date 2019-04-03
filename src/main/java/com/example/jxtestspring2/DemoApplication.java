56848d2b5ed845301b9606e6b631ae3971678534
testovy-git-user <aaa>
2019-04-03 14:41:02 +0200

    Added dummy comment functionality

--- src/main/java/com/example/jxtestspring2/DemoApplication.java
+++ src/main/java/com/example/jxtestspring2/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.jxtestspring2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// dummy comment
 @SpringBootApplication
 public class DemoApplication {
 
